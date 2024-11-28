ac823a9149ff8b351c1f8bf2e9ea8d10d6dbbdde
IAMBH <xz7840@dgu.edu>
2024-11-28 13:24:19 +0900

    Feat: 카테고리 추가

--- BE/suggestion/apps.py
+++ BE/suggestion/apps.py
@@ -1,28 +1,28 @@
 from django.apps import AppConfig
+from keybert import KeyBERT
+
 from sentence_transformers import SentenceTransformer
 import faiss
 import os
 import pdfplumber
 import numpy as np
-from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
-import torch
-from keybert import KeyBERT
 import re
 
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-pdf_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../상품요약서'))
-summarization_model_name = "snunlp/KR-Summarization"
+pdf_dirs = {
+    "실손보험": os.path.abspath(os.path.join(os.path.dirname(__file__), '../../상품요약서/실손보험')),
+    "건강보험": os.path.abspath(os.path.join(os.path.dirname(__file__), '../../상품요약서/건강보험(암 등)')),
+    "종신보험": os.path.abspath(os.path.join(os.path.dirname(__file__), '../../상품요약서/종신보험')),
+    "정기보험": os.path.abspath(os.path.join(os.path.dirname(__file__), '../../상품요약서/정기보험')),
+    "기타": os.path.abspath(os.path.join(os.path.dirname(__file__), '../../상품요약서/기타')),
+}
 
-texts = []
-texts_and_filenames = []
-embeddings = None
-index = None
 model = None
-kw_model = None
 
-generator = None
+# 데이터 저장을 위한 전역 변수
+rag_models = {}
 
 # 텍스트 전처리 함수
 def clean_text(text):
@@ -45,8 +45,8 @@ def extract_texts_and_filenames(pdf_dir):
                 texts_and_filenames.append((cleaned_text, filename))  # 텍스트와 파일 이름 저장
     return texts_and_filenames
 
-def initialize_rag_model():
-    global texts, texts_and_filenames, embeddings, index, model, generator, kw_model
+def initialize_rag_model(pdf_dir):
+    global model 
     
     # PDF에서 텍스트와 파일 이름 추출
     texts_and_filenames = extract_texts_and_filenames(pdf_dir)
@@ -54,8 +54,10 @@ def initialize_rag_model():
     filenames = [item[1] for item in texts_and_filenames]
 
     # 텍스트 임베딩 생성
-    model = SentenceTransformer('all-MiniLM-L6-v2')
+    model = SentenceTransformer('jhgan/ko-sroberta-multitask')
     embeddings = model.encode(texts, normalize_embeddings=True)
+    
+    # KeyBERT 모델 생성
     kw_model = KeyBERT(model)
     
     # FAISS 인덱스 설정
@@ -63,21 +65,26 @@ def initialize_rag_model():
     index = faiss.IndexFlatIP(embedding_dim)
     index.add(np.array(embeddings))
     
-    # 한국어 생성 모델 설정
-    # try:
-    #     tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
-    #     summarization_model = AutoModelForCausalLM.from_pretrained(
-    #         "facebook/bart-large-cnn",
-    #         torch_dtype=torch.float16  # 메모리 최적화
-    #     )
-    #     generator = pipeline("text-generation", model=summarization_model, tokenizer=tokenizer, device=-1)
-    # except Exception as e:
-    #     print(f"요약 모델 로드 중 오류 발생: {e}")
-    #     generator = None
+    return {
+        # "texts": texts,
+        # "filenames": filenames,
+        "texts_and_filenames" : texts_and_filenames,
+        "embeddings": embeddings,
+        "index": index,
+        # "model": model,
+        "kw_model": kw_model
+    }
 
 class SuggestionConfig(AppConfig):
     name = 'suggestion'
 
     def ready(self):
-        initialize_rag_model()
+        global rag_models
+        
+        # 각 보험 종류에 대해 RAG 모델 초기화
+        for insurance_type, dir_path in pdf_dirs.items():
+            if os.path.exists(dir_path):  # 폴더가 존재하는 경우에만 초기화
+                rag_models[insurance_type] = initialize_rag_model(dir_path)
+            else:
+                print(f"폴더가 존재하지 않습니다: {dir_path}")
         
