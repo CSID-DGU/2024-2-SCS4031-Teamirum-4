bc2eaa7427f923ddc1207183df634e4f7b62d806
IAMBH <xz7840@dgu.edu>
2024-11-30 17:23:42 +0900

    Fix: pdf 이름 추출

--- BE/suggestion/views.py
+++ BE/suggestion/views.py
@@ -127,7 +127,7 @@ class SuggestionAPIView(APIView):
 
                 # 추천 결과를 딕셔너리로 저장
                 recommendation = {
-                    'product_name': product_name[:-4],
+                    'product_name': product_name,
                     'summary_text': rec_text,
                     'similarity_score': similarity_score,
                     'reason': reason
@@ -136,7 +136,7 @@ class SuggestionAPIView(APIView):
                 recommendation_results.append(recommendation)
                 
                 recommendation = {
-                    'product_name': product_name,
+                    'product_name': product_name[:-4],
                     'reason': reason
                 }
                 
