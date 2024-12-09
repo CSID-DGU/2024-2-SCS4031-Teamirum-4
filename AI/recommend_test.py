d7cb2d606fc6be961e9a4146298dadbad8e92ddb
IAMBH <xz7840@dgu.edu>
2024-12-09 23:26:10 +0900

    Feat: test

--- AI/recommend_test.py
+++ AI/recommend_test.py
@@ -6,15 +6,25 @@ from sklearn.preprocessing import MinMaxScaler
 from sentence_transformers import SentenceTransformer
 import numpy as np
 import json
+import time
+
 
 # 환경 변수 설정
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+pdf_dirs = {
+    "실손보험": os.path.abspath(os.path.join(os.path.dirname(__file__), '../상품요약서/실손보험')),
+    "건강보험": os.path.abspath(os.path.join(os.path.dirname(__file__), '../상품요약서/건강보험(암 등)')),
+    "종신보험": os.path.abspath(os.path.join(os.path.dirname(__file__), '../상품요약서/종신보험')),
+    "정기보험": os.path.abspath(os.path.join(os.path.dirname(__file__), '../상품요약서/정기보험')),
+    "기타": os.path.abspath(os.path.join(os.path.dirname(__file__), '../상품요약서/기타')),
+}
 
 # 추천 결과를 저장할 리스트
 recommendation_results = []
 
 model = SentenceTransformer("jhgan/ko-sroberta-multitask")
-pdf_dir = "/Users/ddinga/Downloads/요약서실손보험"
+# pdf_dir = "/Users/ddinga/Downloads/요약서실손보험"
 
 # PDF에서 텍스트 추출 함수
 def extract_texts_and_filename(pdf_dir):
@@ -32,18 +42,18 @@ def extract_texts_and_filename(pdf_dir):
     return texts_and_filenames
 
 # 사용자 입력 정의
-user_input = {
-    "성별": "남성",
-    "나이": "43",
-    "흡연여부": "예",
-    "음주빈도": "없음",
-    "운동빈도": "없음",
-    "부양가족여부": "있음",
-    "연소득(만원)": 6000,
-    "가입목적": "실손보험",
-    "선호보장기간": 10,
-    "보험료납입주기": "분기납",
-}
+# user_input = {
+#     "성별": "남성",
+#     "나이": "43",
+#     "흡연여부": "예",
+#     "음주빈도": "없음",
+#     "운동빈도": "없음",
+#     "부양가족여부": "있음",
+#     "연소득(만원)": 6000,
+#     "가입목적": "실손보험",
+#     "선호보장기간": 10,
+#     "보험료납입주기": "분기납",
+# }
 
 # 나이 등급화 함수
 def get_age_group(age):
@@ -59,9 +69,36 @@ def get_age_group(age):
     else:
         return '60대 이상'
     
-# 나이를 계급화
-if "나이" in user_input:
-    user_input["나이"] = get_age_group(user_input["나이"])
+def json_to_query(data):
+    result = {}
+    for category, details in data.items():
+        for key, value in details.items():
+            if key == '생년월일':
+                birth_year, birth_month, birth_day = map(int, value.split('-'))
+                age = time.localtime().tm_year - birth_year
+                if age < 20:
+                    age_group = '10대'
+                elif age < 30:
+                    age_group = '20대'
+                elif age < 40:
+                    age_group = '30대'
+                elif age < 50:
+                    age_group = '40대'
+                elif age < 60:
+                    age_group = '50대'
+                else:
+                    age_group = '60대 이상'
+                result["나이"] = age_group
+            
+            elif key == '카테고리':
+                continue  # 카테고리는 포함하지 않음
+            
+            elif key == '선호보장기간':
+                result['선호보장기간'] = 10
+            
+            else:
+                result[key] = value
+    return result
 
 # 숫자 필드와 텍스트 필드 구분
 numeric_fields = ["연소득(만원)", "선호보장기간"]
@@ -142,22 +179,42 @@ def recommend_top_k_products(user_input, pdf_dir, model, k=3):
     return results[:k]
 
 # 추천 실행
-top_3_recommendations = recommend_top_k_products(user_input, pdf_dir, model, k=3)
 
-# 결과 출력
-for idx, rec in enumerate(top_3_recommendations):
-    print(f"Top {idx+1}: {rec['상품명']}, 총점: {rec['총점']:.2f}")
-    print(f"추천 이유: {rec['추천 이유']}")
-    print("------")
+with open(os.path.abspath(os.path.join(os.path.dirname(__file__), '../random_data.json')), 'r', encoding='utf-8') as f:
+    user_inputs = json.load(f)
 
-    recommendation = {
-        'product_name': rec['상품명'],
-        'similarity_score': rec['총점'],
-        'reason': rec['추천 이유']
+recommendation_results = []
+
+for user_input in user_inputs:
+    
+    category = user_input["가입목적및개인선호"].get("카테고리")  # 카테고리 추출
+    pdf_dir = pdf_dirs.get(category)
+    user_query = json_to_query(user_input)
+    print(user_query)
+    
+    top_3_recommendations = recommend_top_k_products(user_query, pdf_dir, model, k=3)
+
+    
+    output_data = {
+        "user_query": user_query,
     }
     
-    recommendation_results.append(recommendation)
+    recommendation_results.append(output_data)
+    
+    # 결과 출력
+    for idx, rec in enumerate(top_3_recommendations):
+        print(f"Top {idx+1}: {rec['상품명']}, 총점: {rec['총점']:.2f}")
+        print(f"추천 이유: {rec['추천 이유']}")
+        print("------")
 
-# 추천 결과를 JSON 파일로 저장
-with open('recommendations.json', 'w', encoding='utf-8') as f:
-    json.dump(recommendation_results, f, ensure_ascii=False, indent=4)
+        recommendation = {
+            'product_name': rec['상품명'],
+            'similarity_score': rec['총점'],
+            # 'reason': rec['추천 이유']
+        }
+        
+
+        recommendation_results.append(recommendation)
+        
+    with open('recommendations_result.json', 'w', encoding='utf-8') as f:
+        json.dump(recommendation_results, f, ensure_ascii=False, indent=4)
